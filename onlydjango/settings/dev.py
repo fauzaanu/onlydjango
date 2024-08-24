a2f49c3352291a90763afdf56cb9425ff6ff6125
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2024-08-24 07:31:41 +0500

    Update dev.py

--- onlydjango/settings/dev.py
+++ onlydjango/settings/dev.py
@@ -9,6 +9,39 @@ ALLOWED_HOSTS = [
 ]
 SECRET_KEY = "1234"
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+# Use any S3 Compatible storage backend for static and media files
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_CUSTOM_DOMAIN = "cdn.careonemaldives.com"
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {},
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "OPTIONS": {
+            "location": path.join(BASE_DIR, "staticfiles"),
+            "base_url": "/staticfiles/",
+        },
+    },
+    "media": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+        "OPTIONS": {},
+    },
+}
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 # POSTGRES
