4cc35708ab9f4b7fbefa9a950802d8da13cd42a8
Fauzaan Gasim <hello@fauzaanu.com>
2025-05-19 17:37:35 +0500

    feat: adds wagtail urls into the project urls.py
    closes #21

--- onlydjango/urls.py
+++ onlydjango/urls.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +14,10 @@ urlpatterns = [
 urlpatterns += [
     path("__debug__/", include(debug_toolbar.urls)),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("accounts/", include("allauth.urls")),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    path('accounts/', include('allauth.urls')),
 
     # apps
     path("", include("sampleapp.urls")),
