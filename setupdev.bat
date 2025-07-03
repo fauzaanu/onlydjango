7c57b709df60d023178700f4b71266e53dc87bcf
Fauzaan <hello@fauzaanu.com>
2025-07-03 21:28:27 +0500

    feat: run makemigrations on setup dev

--- setupdev.bat
+++ setupdev.bat
@@ -1,3 +1,4 @@
 for /f %%i in ('docker ps -q') do docker rm -f %%i
 docker compose -f dev_helpers/dev.docker-compose.yml up -d
+uv run python manage.py makemigrations
 uv run python manage.py migrate
